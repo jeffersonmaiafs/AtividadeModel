0fbb65636fa631fcb15a9e396e078009fa95a441
jeffersonmaiafs <jeffersonmaia.si@gmail.com>
2017-04-30 03:21:23 -0300

    2 avaliação - Laravel
    Avaliação 2

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -10,7 +10,35 @@
 | and give it the controller to call when that URI is requested.
 |
 */
+//
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+//
+//Route::get('/', function () {
+//    return view('welcome');
+//});
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Route::get('/', 'WelcomeController@index');
+
+
+//Testando este no momento
+Route::get('exemplo', 'WelcomeController@exemplo');
+
+//Direciona para o index através do caminho localhost:8000/admin/categories
+Route::get('admin/categories', 'AdminCategoriesController@index');
+
+//Direciona para o index através do caminho localhost:8000/admin/categories
+Route::get('admin/products', 'AdminProductsController@index');
+
+Route::get('home', 'HomeController@index');
+
+Route::controllers([
+    'auth'     => 'Auth\AuthController',
+    'password' => 'Auth\PasswordController',
+]);    
+    
+
+
+
